a08b308877ef0f4e765944afacd4e588bcb404cc
Richard <richardlwhittington@gmail.com>
2024-07-02 15:13:41 -0500

    add js alert

--- main.js
+++ main.js
@@ -3,4 +3,5 @@ console.log("Hello, World!");
 const saints = ['Patrick', 'Columba', 'Bridget', 'Andrew'];
 for(let i = 0;i < saints.length;i++){
 console.log(saints[i]);
+alert('Elementary my dear Watson');
 };
