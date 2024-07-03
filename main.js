aad5e873ad913ab6976209a88a08627296dbc59a
Richard <richardlwhittington@gmail.com>
2024-07-03 15:46:34 -0500

    add new func

--- main.js
+++ main.js
@@ -5,3 +5,11 @@ for(let i = 0;i < saints.length;i++){
 console.log(saints[i]);
 alert('Elementary my dear Watson');
 };
+
+const reality = () =>{
+    if('money into checking on Friday' === true){
+        console.log("All is good!");
+    } else {
+        console.log("I am screwed!");
+    }
+}
